a4203d88d039c3ff34596d485b1b6f834889a3f3
sdac <sdac@sdac-PC>
2023-05-27 15:32:01 +0530

    update2.2

--- school/src/school/School.java
+++ school/src/school/School.java
@@ -3,7 +3,8 @@ package school;
 public class School {
 public static void main(String[] args) {
 	System.out.println("school");
-	System.err.println("hello");
+	System.out.println("hello");
 	System.out.println("hello java");
+	System.out.println("hello world");
 }
 }
